9c9b6072f1f87917bb9e76ba9665fe3a2b6ca247
yunhaDev <yunhaDev98@gmail.com>
2023-03-09 23:50:36 +0900

    Create CutPapper.java

---
+++ src/main/java/Level_0/CutPapper.java
@@ -0,0 +1,8 @@
+class CutPapper {
+    public static int solution(int M, int N) {
+        return M*N-1;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(3,4));
+    }
+}
