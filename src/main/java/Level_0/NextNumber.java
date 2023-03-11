c024a4bffd2a74f1eb1a356de904c9634ee26e03
JeongYunha <yunhadev98@gmail.com>
2023-03-11 13:49:36 +0900

    feat: 다음에 올 숫자

--- src/main/java/Level_0/NextNumber.java
+++ src/main/java/Level_0/NextNumber.java
@@ -1,24 +1,44 @@
 package Level_0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/120924
 // 다음에 올 숫자
 class NextNumber {
-    public static int solution(int[] common) {
-        int answer = 0;
-        int num = 0;
-        int length = common.length;
-        if (length > 2) {
-            if((common[2]-common[1])==(common[1]-common[0])) {
-                num = common[2]-common[1];
-                answer = common[length-1] + num;
-            }else if((common[2]/common[1])==(common[1]/common[0])) {
-                num = common[2]/common[1];
-                answer = common[length-1] * num;
+        public static List<Integer> solution(int num, int total) {
+            List<Integer> answer = new ArrayList<>();
+            boolean even = false;
+            int sum, mid, count = 0;
+            if(num%2==0) {
+                even = true;
             }
-        }
 
-        return answer;
-    }
+            if(even){
+                sum = total/(num/2); // 7
+                mid = sum/2; // 3
+                count = num/2; // 2
+                for(int i=count-1;i>0;i--) {
+                    answer.add(mid-i);
+                }
+                answer.add(mid);
+                for(int i=1;i<=count;i++) {
+                    answer.add(mid+i);
+                }
+            } else {
+                mid = total/num;
+                count = (num-1)/2; // 2
+                for(int i=count;i>0;i--){
+                    answer.add(mid-i);
+                }
+                answer.add(mid);
+                for(int i=1;i<=count;i++){
+                    answer.add(mid+i);
+                }
+            }
+
+            return answer;
+        }
 
 //    public int solution(int[] common) {
 //        int answer = 0;
@@ -34,6 +54,6 @@ class NextNumber {
 
     public static void main(String[] args) {
         int common[] = {2, 4, 8};
-        System.out.println(solution(common));
+        System.out.println(solution(4,14));
     }
 }
